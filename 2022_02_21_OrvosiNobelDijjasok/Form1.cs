d3e1d5c8d5ea718a46ae04c336903c63c2efeddb
janosibence29 <92314900+janosibence29@users.noreply.github.com>
2022-02-21 10:18:15 +0100

    Add project files.

---
+++ 2022_02_21_OrvosiNobelDijjasok/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _2022_02_21_OrvosiNobelDijjasok
+{
+    public partial class btnAdatokmentese : Form
+    {
+        
+        public btnAdatokmentese()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
